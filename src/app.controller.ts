f7647834b3b4a88fee796ad14ad704d3c519f241
shon5544 <sbs8239@gmail.com>
2024-11-22 05:09:23 +0900

    feat: add health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,14 @@
-import { Body, Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UserHelloRequest } from './user.request';
-import { UserHelloResponse } from './user.response';
+import { HealthCheckResponse } from './user.response';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('body-test')
+  @Get('health-check')
   @HttpCode(HttpStatus.OK)
-  getHello2(@Body() request: UserHelloRequest): UserHelloResponse {
-    const userName: string = request.name;
-
-    return new UserHelloResponse(userName);
+  checkHealth(): HealthCheckResponse {
+    return new HealthCheckResponse();
   }
 }
--- src/user.request.ts
+++
@@ -1,3 +0,0 @@
-export class UserHelloRequest {
-  name: string;
-}
