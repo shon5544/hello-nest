a1d84ab06790252c9906410636bfb691781e68b4
shon5544 <sbs8239@gmail.com>
2024-11-25 00:41:16 +0900

    feat: article 수정 API 추가

--- src/article/application/article.controller.ts
+++ src/article/application/article.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { CreateArticleResponse } from './article.response';
+import { Body, Controller, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
+import { CreateArticleResponse, UpdateArticleResponse } from './article.response';
 import { ArticleService } from '../domain/article.service';
-import { CreateArticleRequest } from './article.request';
-import { CreateArticleResult } from '../domain/article.result';
+import { CreateArticleRequest, UpdateArticleRequest } from './article.request';
+import { CreateArticleResult, UpdateArticleResult } from '../domain/article.result';
 
 @Controller('articles')
 export class ArticleController {
@@ -10,7 +10,7 @@ export class ArticleController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async createBlog(
+  async createArticle(
     @Body() request: CreateArticleRequest,
   ): Promise<CreateArticleResponse> {
     const result: CreateArticleResult = await this.articleService.create(
@@ -19,5 +19,20 @@ export class ArticleController {
     );
 
     return new CreateArticleResponse(result, '성공적으로 생성됐습니다.');
+  }
+
+  @Put(":id")
+  @HttpCode(HttpStatus.OK)
+  async updateArticle(
+    @Body() request: UpdateArticleRequest,
+    @Param('id') id: number,
+  ): Promise<UpdateArticleResponse> {
+    const result: UpdateArticleResult = await this.articleService.update(
+      id,
+      request.title,
+      request.content,
+    );
+
+    return new UpdateArticleResponse(result, '성공적으로 수정됐습니다.');
   }
 }
--- src/article/application/article.request.ts
+++ src/article/application/article.request.ts
@@ -2,3 +2,8 @@ export class CreateArticleRequest {
   title: string;
   content: string;
 }
+
+export class UpdateArticleRequest {
+  title: string;
+  content: string;
+}
